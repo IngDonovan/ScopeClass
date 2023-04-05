07800b87ecb5c8ba53832c79a0610d269c37e356
Donovan Rojas <rojasdrm03@gmail.com>
2023-04-05 10:35:18 -0500

    Reasign and ReDeclar var let const arrays

---
+++ src/scope/re.js
@@ -0,0 +1,33 @@
+var firstName; //Undefined declaramos
+firstName = 'Oscar';//asignamos
+console.log(firstName);
+
+var lastName = "David";//declaramos y asignamos de una vez
+lastName = 'Ana'; //reasignar
+console.log(lastName);
+
+var secondName = "David";//declarando / asignando
+var secondName = "Ana";//reAsignando
+console.log(secondName);
+
+//let
+let fruit = 'Apple'; // Declarando y asignando
+fruit = 'Kiwi'; // reAsignar
+console.log(fruit);
+
+// let fruit = 'Banana'; //no es posible
+
+//const
+const animal = 'dog';
+// animal = 'cat';//no va a dejar reAsignar
+//const animal = 'Snake'; //tampoco puedo hacer
+console.log(animal); 
+
+const vehicles = [];
+vehicles.push("👻");
+console.log(vehicles);
+
+vehicles.pop();
+console.log(vehicles);
+
+
