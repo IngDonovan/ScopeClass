f3d306caf9a831b0f889d10093b0832dfd1304b2
Donovan Rojas <rojasdrm03@gmail.com>
2023-04-05 10:43:26 -0500

    mode strict example

--- src/scope/strict.js
+++ src/scope/strict.js
@@ -1,5 +1,5 @@
 //internamente hizo var pi; undefined
 //pero si le hacemos el modo avanzado
-'use strict';
+'use strict'; 
 pi = 3.1416;
-console.log(pi);
+console.log(pi);//en este caso no esta definido y esta funcion hace que cumplamos con las reglas
