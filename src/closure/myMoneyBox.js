7d702d9b46fba5f16968161b854139f07c6ca1c8
Donovan Rojas <rojasdrm03@gmail.com>
2023-04-07 08:52:25 -0500

    Ejercicio with closure

--- src/closure/myMoneyBox.js
+++ src/closure/myMoneyBox.js
@@ -1,8 +1,29 @@
-function moneyBox(coins) {
+// function moneyBox(coins) {
+//     let saveCoins = 0;
+//     saveCoins += coins;
+//     console.log(`MoneyBox: $${saveCoins}`);
+// }
+
+// moneyBox(5);
+// moneyBox(5);
+
+//toca crear una closure para arreglar el moneybox
+
+function moneyBox() {
     let saveCoins = 0;
-    saveCoins += coins;
-    console.log(`MoneyBox: $${saveCoins}`);
+    function countCoins(coins) {
+        saveCoins += coins;
+        console.log(`MoneyBox $${saveCoins}`);        
+    }
+    return countCoins;
 }
 
-moneyBox(5);
-moneyBox(5);
+const myMoneyBox = moneyBox();
+myMoneyBox(5);
+myMoneyBox(5);
+myMoneyBox(15);
+
+const moneyBoxAna = moneyBox();
+moneyBoxAna(10);
+moneyBoxAna(20);
+moneyBoxAna(5);
